bce239a4855e76911df575ed3e2b5d62e7f091fc
jacoobanderson <j00cwa@gmail.com>
2022-05-21 23:27:24 +0200

    Make nav responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,22 @@ interface CurrentProps {
 const Navbar = ({ current }: CurrentProps) => {
   return (
       <div className='flex text-gray-800 justify-between'>
-          <div className='flex m-5 h-12 justify-center items-center'>
+          {/* Desktop version */}
+          <div className='hidden sm:flex m-5 h-12 justify-center items-center'>
               <Link className={current === 'Home' ? 'border-b border-solid border-gray-800 mx-6' : 'mx-6'} to='/'>Home</Link>
               <Link className={current === 'Products' ? 'border-b border-solid border-gray-800 mx-6' : 'mx-6'} to=''>Products</Link>
               <Link className={current === 'Inspiration' ? 'border-b border-solid border-gray-800 mx-6' : 'mx-6'} to=''>Inspiration</Link>
               <Link className={current === 'About' ? 'border-b border-solid border-gray-800 mx-6' : 'mx-6'} to=''>About</Link>
           </div>
-          <div className='flex justify-center items-center'>
+          {/* Mobile version */}
+          <div className='flex sm:hidden justify-center items-center m-5 h-12'>
+              <MenuIcon className='h-8 w-8 mx-6' />
+          </div>
+          <div className='flex justify-center items-center m-5 h-12'>
               <ShoppingCartIcon className='h-8 w-8 mx-6'/>
           </div>
       </div>
+    
   )
 }
 
