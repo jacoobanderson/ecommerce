84fdc50b0de05a6366846933dfec29c8713caa7e
jacoobanderson <j00cwa@gmail.com>
2022-05-22 00:18:04 +0200

    Make shopping cart responsive

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ShoppingCart = () => {
   return (
-    <div className="border border-solid border-[#444242] flex flex-col justify-between absolute w-96 h-96 bg-white right-20 top-20 text-sm">
-        <div className='border-b border-solid border-[#444242] text-center p-3 text-base'>Shopping cart</div>
+    <div className="top-20 w-72 left-auto right-auto flex flex-col border border-solid border-[#444242] justify-between absolute sm:w-96 h-96 bg-white sm:right-20 sm:top-20 text-sm">
+        <div className='border-b border-solid border-[#444242] text-center p-3 text-base '>Shopping cart</div>
         <button className='flex-end bg-[#444242] text-white p-2 tracking-wider'>ORDER</button>
     </div>
   )
